8181a4e97ed6faa0c2612e9ddae1aabefce48d2d
dongshengzhang <zhang198058@hotmail.com>
2017-11-30 14:16:55 +0800

    更改手工获取注入获取

--- model/src/main/java/org/interestTeam/model/ApplicationContextProvider.java
+++ model/src/main/java/org/interestTeam/model/ApplicationContextProvider.java
@@ -8,17 +8,59 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationContextProvider implements ApplicationContextAware {
 
-	private static ApplicationContext context;
-
 	private ApplicationContextProvider() {
 	}
 
+	/**
+	 * 上下文对象实例
+	 */
+	private static ApplicationContext applicationContext;
+
+	@SuppressWarnings("static-access")
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		context = applicationContext;
+		this.applicationContext = applicationContext;
 	}
 
-	public static <T> T getBean(String name, Class<T> aClass) {
-		return context.getBean(name, aClass);
+	/**
+	 * 获取applicationContext
+	 * 
+	 * @return
+	 */
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	/**
+	 * 通过name获取 Bean.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static Object getBean(String name) {
+		return getApplicationContext().getBean(name);
+	}
+
+	/**
+	 * 通过class获取Bean.
+	 * 
+	 * @param clazz
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> T getBean(Class<T> clazz) {
+		return getApplicationContext().getBean(clazz);
+	}
+
+	/**
+	 * 通过name,以及Clazz返回指定的Bean
+	 * 
+	 * @param name
+	 * @param clazz
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> T getBean(String name, Class<T> clazz) {
+		return getApplicationContext().getBean(name, clazz);
 	}
 
 }
