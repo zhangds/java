889be277f000e418d696dcf179845771482a2de8
dongshengzhang <zhang198058@hotmail.com>
2017-12-03 00:45:21 +0800

    删除无用引用

--- model/src/main/java/org/interestTeam/model/configure/CustomWebMvcConfigurerAdapter.java
+++ model/src/main/java/org/interestTeam/model/configure/CustomWebMvcConfigurerAdapter.java
@@ -2,7 +2,6 @@ package org.interestTeam.model.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
