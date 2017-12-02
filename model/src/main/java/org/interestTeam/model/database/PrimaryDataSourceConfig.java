210179a98d0c3f941bd2ea51f71ffa88688afd01
dongshengzhang <zhang198058@hotmail.com>
2017-12-03 00:51:04 +0800

    删除引用

--- model/src/main/java/org/interestTeam/model/database/PrimaryDataSourceConfig.java
+++ model/src/main/java/org/interestTeam/model/database/PrimaryDataSourceConfig.java
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * @author dongshengzhang
