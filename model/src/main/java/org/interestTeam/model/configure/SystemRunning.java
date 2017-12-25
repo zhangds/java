6413c27b945b4e65a22a5255248b4b556e205f15
dongshengzhang <zhang198058@hotmail.com>
2017-12-25 17:00:10 +0800

    统一项目参数读取

---
+++ model/src/main/java/org/interestTeam/model/configure/SystemRunning.java
@@ -0,0 +1,43 @@
+/**   
+ * @Title: SystemRunning.java 
+ * @Package org.interestTeam.model.configure 
+ * @Description: TODO
+ * @author zhangds zhang198058@hotmail.com   
+ * @date 2017年12月25日 下午4:47:35 
+ * @version V1.0  
+ */
+package org.interestTeam.model.configure;
+
+import java.util.regex.Pattern;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+/** 
+ * @ClassName: SystemRunning 
+ * @Description: TODO
+ * @author zhangds zhang198058@hotmail.com 
+ * @date 2017年12月25日 下午4:47:35 
+ *  
+ */
+@ConfigurationProperties(prefix="project")
+@Configuration
+@Data
+public class SystemRunning {
+
+	String pageSize ;
+	
+	public int getPageSize(){
+		Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+		if (pageSize != null && pattern.matcher(pageSize).matches()){
+			return Integer.valueOf(pageSize);
+		}
+		return 20;
+	}
+	
+	String redirect;
+	String name;
+	String defaultKey;
+}
