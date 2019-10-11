aebf706760dc2a124ead11e962b98bffdd67ff54
zhangds <zhang198058@hotmail.com>
2019-10-12 02:26:15 +0800

    dsfsd

---
+++ springboot2/src/main/java/com/ctcc/kf/springboot2/configure/AsyncTaskConfig.java
@@ -0,0 +1,39 @@
+package com.ctcc.kf.springboot2.configure;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncTaskConfig implements AsyncConfigurer{
+	@Bean
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+        //设置核心线程数
+        threadPool.setCorePoolSize(10);
+        //设置最大线程数
+        threadPool.setMaxPoolSize(200);
+        //线程池所使用的缓冲队列
+        threadPool.setQueueCapacity(30);
+        //等待任务在关机时完成--表明等待所有线程执行完
+        threadPool.setWaitForTasksToCompleteOnShutdown(true);
+        // 等待时间 （默认为0，此时立即停止），并没等待xx秒后强制停止
+        threadPool.setAwaitTerminationSeconds(1000);//60
+        //  线程名称前缀
+        threadPool.setThreadNamePrefix("kf-Async-");
+        // 初始化线程
+        threadPool.initialize();
+        return threadPool;
+	}
+
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return null;
+	}
+
+}
