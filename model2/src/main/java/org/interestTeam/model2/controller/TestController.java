4ab44c3c7cf3cc6637d3216fe71d8de5be6aca64
zhangds <zhang198058@hotmail.com>
2019-10-15 17:19:49 +0800

    模板导入

---
+++ model2/src/main/java/org/interestTeam/model2/controller/TestController.java
@@ -0,0 +1,22 @@
+package org.interestTeam.model2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping(value = "/test")
+@Slf4j
+public class TestController {
+
+	@ApiOperation(value = "解密", notes = "字符串解密")
+	@GetMapping(value="/decrypt/{pwd}")
+	public String decrypt(@PathVariable String pwd){
+		log.debug("首次!");
+		return "index/index";
+	}
+}
