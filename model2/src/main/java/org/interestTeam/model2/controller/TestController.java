224782b340f6e45e4bab32851484655ce9e54b05
zhangds <zhang198058@hotmail.com>
2019-10-22 18:06:03 +0800

    test

--- model2/src/main/java/org/interestTeam/model2/controller/TestController.java
+++ model2/src/main/java/org/interestTeam/model2/controller/TestController.java
@@ -1,9 +1,9 @@
 package org.interestTeam.model2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +15,10 @@ public class TestController {
 
 	@ApiOperation(value = "解密", notes = "字符串解密")
 	@GetMapping(value="/test/decrypt/{pwd}")
-	public String decrypt(@PathVariable String pwd){
+	public String decrypt(@PathVariable String pwd,Model model){
 		log.debug("首次!");
+		model.addAttribute("projectName", "demo");
 		return "index/index";
 	}
-	@GetMapping(value="/error/404")
-	@ResponseBody
-	public String erorr404(){
-		return "index/index";
-	}
+
 }
